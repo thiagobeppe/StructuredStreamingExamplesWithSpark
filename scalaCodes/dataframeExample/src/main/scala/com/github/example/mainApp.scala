715fea72256b43fe715d70f28e315a1b93f722d1
thiagobeppe <thiagoabeppe@gmail.com>
2020-05-11 06:51:18 -0300

    adding some manipulations

--- scalaCodes/dataframeExample/src/main/scala/com/github/example/mainApp.scala
+++ scalaCodes/dataframeExample/src/main/scala/com/github/example/mainApp.scala
@@ -1,10 +1,12 @@
 package com.github.example
 
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.{lit, expr}
 
 import com.github.example.utils.utils.setupLogging
 
 object mainApp extends App {
+
   val spark = SparkSession
     .builder
       .master("local")
@@ -12,6 +14,7 @@ object mainApp extends App {
     .getOrCreate()
 
   setupLogging()
+
 
   val data = spark
                 .read
@@ -22,4 +25,17 @@ object mainApp extends App {
                 .load("./../../data/bank-additional-full.csv")
   println(data.printSchema())
 
+  println(data.select("age").show(5))
+
+  //Criaçao de uma nova coluna
+  val data_2 = data.withColumn("A_New_Column", lit(1))
+  println(data_2.printSchema())
+  println(data_2.select("A_New_Column").show(10))
+
+  val test = expr("age > 40")
+  //Adding a new columns with some filter
+  val data_with_teste = data.select("age", "y").withColumn("teste", test)
+  val new_data = data_with_teste.selectExpr("teste as MoreThan40")
+  data_2.drop("A_New_Column")
+  println(new_data.printSchema())
 }
